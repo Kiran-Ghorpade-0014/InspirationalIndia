6822615a1a30de05eda54d2ec4e137a1caa70d1c
kiran ghorpade <kiranghorpade0014@gmail.com>
2023-12-19 13:59:49 +0530

    mistake resolved . Mistake : spring boot workspace was in different folder.

--- inspirational_india_Backend/InspirationalIndia/src/main/java/com/example/demo/repository/LikeDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.LikeDetails;
-
-public interface LikeDetailsRepository extends JpaRepository<LikeDetails, Integer>{
-
-}
--- inspirational_india_Backend/InspirationalIndia/src/main/java/com/example/demo/repository/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Region;
-
-public interface RegionRepository extends JpaRepository<Region, Integer>{
-
-}
